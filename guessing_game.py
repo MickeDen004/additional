a2a3bf2dc6325bef51bcfd7a4285297df424ddf7
nikden34562 <nikolasmongrail@gmail.com>
2021-12-11 22:53:54 +0300

    added count

--- guessing_game.py
+++ guessing_game.py
@@ -1,15 +1,23 @@
 import random
 
+
 name = input("Hi! What's your name?")
-computer_number = random.randint(-100,100)
+computer_number = random.randint(-100, 100)
+count = 0
 while True:
     guess = int(input(f"Ok. Print your guess from -100 to 100, {name}"))
     if guess > computer_number:
         print('Its smaller')
+        count += 1
     elif guess < computer_number:
         print('Its bigger ')
-else:
-    print('You win')
+        count += 1
+    elif guess == computer_number:
+        print(f'You win, attempts: {count}')
+        print('Thanks for the game')
+        break
+
+
 
 
 
